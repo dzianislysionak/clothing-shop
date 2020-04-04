e764c495e8154ce375cf94cf7f9303caf51feee8
Dzianis Lysionak <minldft@gmail.com>
2020-04-04 13:58:55 +0300

    adding homepage adding sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-
 import "./App.css";
+import HomePage from "./homepage.component";
 
 function App() {
-  return <div></div>;
+  return <HomePage />;
 }
 
 export default App;
